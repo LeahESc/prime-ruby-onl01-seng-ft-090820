c98907a55392db8ad6f29d48aa2e4c1ae9c01bae
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 21:13:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(integer)
-  numbers = (2..Math.sqrt(integer))
   if integer <= 1 
     false 
   elsif integer == 2 || integer == 3 
     true 
   else 
+    numbers = (2..Math.sqrt(integer))
     numbers_array = numbers.to_a 
     if numbers_array.find {|factor| integer % factor == 0}
       false 
