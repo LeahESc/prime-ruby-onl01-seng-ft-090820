1f5cd88032ce34ba13c851f1b0396fb2f9584582
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 21:06:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,9 @@
 def prime?(integer)
   numbers = (2...integer.sqrt(integer))
+  if integer <= 1 
+    false 
+  elsif integer == 2 || integer == 3 
+    true 
+  else 
+    numbers_array = numbers.a 
+    if numbers_array.find 
