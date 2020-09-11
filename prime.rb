8a58fc246e020284b5ef701fadb028edc37a4b6c
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 21:14:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1 
+    false 
+  elsif integer == 2 || integer == 3 
+    true 
+  else 
+    numbers = (2..Math.sqrt(integer))
+    numbers_array = numbers.to_a 
+    if numbers_array.find {|factor| integer % factor == 0}
+      false 
+    else 
+      true 
+    end
+  end 
+end
